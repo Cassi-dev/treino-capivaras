2ef69518537ec9805ceb86d3c3f278d1788928db
Cassi-dev <164269151+Cassi-dev@users.noreply.github.com>
2026-08-05 07:51:10 -0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "treino-capivaras-v1";
+const CACHE_NAME = "treino-capivaras-v2";
 const APP_SHELL = ["./", "./index.html", "./app.js", "./styles.css", "./manifest.json", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", (event) => {
